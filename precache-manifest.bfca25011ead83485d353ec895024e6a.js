c5df8bdc5b2028040026d935ea682d661baeb1b9
UsamaSaleem148 <usamasaleem674@gmail.com>
2020-07-18 21:33:03 +0500

    Updates

---
+++ precache-manifest.bfca25011ead83485d353ec895024e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da0b4151081c6e7231a9c2a48553be2",
+    "url": "/reactcalculator/index.html"
+  },
+  {
+    "revision": "9188be6c4db07f969408",
+    "url": "/reactcalculator/static/css/main.0b118c45.chunk.css"
+  },
+  {
+    "revision": "85432924b8d14bba28bd",
+    "url": "/reactcalculator/static/js/2.10ce1d8d.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/reactcalculator/static/js/2.10ce1d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9188be6c4db07f969408",
+    "url": "/reactcalculator/static/js/main.54bf89c0.chunk.js"
+  },
+  {
+    "revision": "ef16c570e2ef59e72211",
+    "url": "/reactcalculator/static/js/runtime-main.ed2ba9d5.js"
+  }
+]);
